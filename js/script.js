164971c58d0ac3e007b5af76f6dbe0f802a1de8b
SanchoRodriges <104025621+SanchoRodriges@users.noreply.github.com>
2022-05-05 16:52:38 +0300

    Первый вариант диплома

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let page = document.querySelector('.page');
+let themeButton = document.querySelector('.menu-show');
+
+themeButton.onclick = function() {
+  themeButton.classList.toggle('active');
+};
